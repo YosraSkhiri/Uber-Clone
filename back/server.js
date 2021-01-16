aa60a0b4e72f426859ecab6f455905f0dbadd2c0
YosraSkhiri <yos.skhiri@gmail.com>
2021-01-16 14:57:01 +0100

    added the database connection function to the server file

--- back/server.js
+++ back/server.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const app = express();
+const dbConnect = require('./config/db');
+
+dbConnect();
 
 const PORT = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-    res.send('hi')
-});
 
 app.listen(PORT, () => {
     console.log(`App is listening on port: ${PORT}`)
