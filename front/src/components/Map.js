cdc8b28c06c8de126ef6c9d317fceb99e42d4480
YosraSkhiri <yos.skhiri@gmail.com>
2021-01-23 05:08:45 +0100

    added socket.io client to the map page

--- front/src/components/Map.js
+++ front/src/components/Map.js
@@ -1,9 +1,13 @@
 import { useState } from 'react';
 import ReactMapGL, { Marker } from 'react-map-gl';
 import { getRTLTextPluginStatus, setRTLTextPlugin } from 'mapbox-gl';
+import { io } from 'socket.io-client';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 const Map = () => {
+    const socket = io('http://localhost:4000', {
+        transports: ['websocket']
+      });
     const [viewport, setViewport] = useState({
         width: "100vw",
         height: "100vh",
@@ -11,6 +15,14 @@ const Map = () => {
         longitude: 10.1647,
         zoom: 13
     });
+
+    socket.on("connect", () => {
+        console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+      });
+
+      socket.on("disconnect", () => {
+        console.log(socket.id); // undefined
+      });
 
     if (getRTLTextPluginStatus() !== 'loaded' && getRTLTextPluginStatus() !== 'unavailable') {
         setRTLTextPlugin('https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-rtl-text/v0.10.1/mapbox-gl-rtl-text.js');
